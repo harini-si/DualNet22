43e7bb2e9065367d97a3ecc29a51c060badc78b2
harini-si <75417475+harini-si@users.noreply.github.com>
2022-04-19 01:39:59 +0530

    Update run_image.py

--- run_image.py
+++ run_image.py
@@ -4,7 +4,7 @@ import pickle
 import torch
 from torch.utils.data import DataLoader
 
-from dn.data import ImageData, MyDS
+from dn.data import BarlowAugment, ImageData, ImageDataDS, MyDS
 from dn.models import DualNet
 from dn.trainer import Trainer
 from dn.utils import deterministic, load_image_data_pickle, logger, metric
@@ -15,11 +15,12 @@ args = parser.parse_args()
 if __name__ == "__main__":
     deterministic(args.seed)
     data = load_image_data_pickle(args.path)
-    train_loader = data.dloader
-    test_loader = DataLoader(
-        data.test_ds, batch_size=args.test_batch_size, shuffle=False
-    )
-    model = DualNet(slow=args.slow_learner, fast=args.fast_learner)
+    train_data = ImageDataDS(data.train_ds, transform=BarlowAugment())
+    test_data = ImageDataDS(data.test_ds)
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=False)
+    test_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=False)
+
+    model = DualNet(args)
     trainer = Trainer(
         model,
         train_loader,
