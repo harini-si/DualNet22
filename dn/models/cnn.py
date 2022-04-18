b1d3a6d759456b918ce4bc5fc99d058409170611
harini-si <75417475+harini-si@users.noreply.github.com>
2022-04-18 06:07:05 +0530

    Create cnn.py

---
+++ dn/models/cnn.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+
+class CNN(nn.Module):
+    def __init__(self, n_class=74):
+        self.n_class = n_class
+
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )  # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 5 * 5, self.n_class)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        x = self.out(x)
+        return x
