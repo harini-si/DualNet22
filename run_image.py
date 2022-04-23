dc82dcf1c7421005df5c70a3d439743925af159f
harini-si <75417475+harini-si@users.noreply.github.com>
2022-04-23 22:22:44 +0530

    Accuracy

--- run_image.py
+++ run_image.py
@@ -76,7 +76,6 @@ if __name__ == "__main__":
         test_data, transforms, num_tasks=args.n_class // args.n_ways
     )
 
-
     model = DualNet(args).to(device)
     CLoss = torch.nn.CrossEntropyLoss()
     KLLoss = torch.nn.KLDivLoss()
@@ -150,15 +149,17 @@ if __name__ == "__main__":
                     loss.backward()
                     opt.step()
         model.eval()
-        mode='test'
+        mode = "test"
         for task_t, te_loader in enumerate(MetaLoader(test_taskset, args, train=False)):
-            if task_t > task: break
-            
+            if task_t > task:
+                break
+            correct, total = 0, 0
             for data, target in te_loader:
                 data, target = data.cuda(), target.cuda()
                 logits = model(data, task_t)
                 loss = F.cross_entropy(logits, target)
                 pred = logits.argmax(dim=1, keepdim=True)
-                correct = pred.eq(target.view_as(pred)).sum().item()
-                acc = correct / len(data)
-                logging.info("Task {} Acc: {:.4f}".format(task_t, acc))
+                correct += pred.eq(target.view_as(pred)).sum().item()
+                total += len(data)
+            acc = correct / total
+            logging.info("Task {} Acc: {:.4f}".format(task_t, acc))
